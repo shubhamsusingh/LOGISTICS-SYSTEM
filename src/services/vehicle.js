73f4f70ecfa3edd9ad5a605069f35307d6c6621a
shubham singh <singhshubham68738@gmail.com>
2026-03-25 22:36:15 +0530

    working

--- src/services/vehicle.js
+++ src/services/vehicle.js
@@ -2,3 +2,6 @@ import axiosInstance from '@/utils/AxiosInterceptor';
 export const vehicleList=async()=>{
     return axiosInstance.get('/vehicleList');
 }
+export const addVehicle=async(payload)=>{
+    return axiosInstance.post('/addVehicle',payload);
+}
